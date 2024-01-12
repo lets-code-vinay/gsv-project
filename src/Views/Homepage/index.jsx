e189767d78c3f7e47d96e963bf2f7ac09c683de8
vinay <vinay.shermauryan@gmail.com>
2024-01-12 13:48:45 +0530

    added lazy loading

--- src/Views/Homepage/index.jsx
+++ src/Views/Homepage/index.jsx
@@ -1,29 +1,30 @@
-import React from "react";
+import React, { lazy } from "react";
 import { useLocation } from "react-router-dom";
 import { Box } from "@material-ui/core";
-
-import Trending from "../../Components/Trending";
-import OurClients from "../../Components/OurClient/OurClients";
-import InfinitySliderContainer from "../../Components/InfinitySliderContainers";
-import MainSlider from "../../Components/MainSlider";
-import Insight from "../../Components/Insight";
-import OurPresence from "../../Components/Presence";
-import Footer from "../../Components/Footer";
-import Technologies from "../../Components/Technologies";
-import Testimonials from "../../Components/Testimonials";
-
 import "./style.css";
+
+const Trending = lazy(() => import("../../Components/Trending"));
+const OurClients = lazy(() => import("../../Components/OurClient/OurClients"));
+const InfinitySliderContainer = lazy(() =>
+  import("../../Components/InfinitySliderContainers")
+);
+const MainSlider = lazy(() => import("../../Components/MainSlider"));
+const Insight = lazy(() => import("../../Components/Insight"));
+const OurPresence = lazy(() => import("../../Components/Presence"));
+const Footer = lazy(() => import("../../Components/Footer"));
+const Technologies = lazy(() => import("../../Components/Technologies"));
+const Testimonials = lazy(() => import("../../Components/Testimonials"));
 
 const Homepage = () => {
   const { pathname } = useLocation();
 
-  const mainRef = React.useRef(null)
+  const mainRef = React.useRef(null);
 
   /**
    * @description Rendering Page to the top
    */
   React.useEffect(() => {
-    mainRef.current.scrollTo(0, 0)
+    mainRef.current.scrollTo(0, 0);
   }, [pathname]);
 
   return (
